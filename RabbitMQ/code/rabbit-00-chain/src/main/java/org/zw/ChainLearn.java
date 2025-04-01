02bf03dfce3d21931a2b02e4c275f74c4e43a682
zhangwen666666 <1813505899@qq.com>
2025-04-01 21:41:57 +0800

    RabbitMQ简介

---
+++ RabbitMQ/code/rabbit-00-chain/src/main/java/org/zw/ChainLearn.java
@@ -0,0 +1,12 @@
+package org.zw;
+
+public class ChainLearn {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setId(10);
+        student.setName("张三");
+        student.setAge(18);
+        student.setAddress("北京");
+        System.out.println(student);
+    }
+}
---
+++ RabbitMQ/code/rabbit-00-chain/src/main/java/org/zw/Student.java
@@ -0,0 +1,52 @@
+package org.zw;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String address;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
